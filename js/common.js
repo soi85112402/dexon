f86f8b1c9516b0edaf30be0da964e40846e7be83
soi85112402 <soi85112402@gmail.com>
2024-05-10 17:15:26 +0900

    bug catch and test

--- js/common.js
+++ js/common.js
@@ -1,142 +1,110 @@
-// var I = function () {
-//   function e() {
-//     !(function (e, t) {
-//       if (!(e instanceof t))
-//         throw new TypeError('Cannot call a class as a function');
-//     })(this, e),
-//       (this.DOM = {
-//         ghostText: '.js-ghost-text',
-//       }),
-//       (this.ghostText = document.querySelectorAll(this.DOM.ghostText)),
-//       this.init();
-//   }
-//   var t, n, i;
-//   return (
-//     (t = e),
-//     (n = [
-//       {
-//         key: 'init',
-//         value: function () {
-//           var e,
-//             r = this;
-//           (null === (e = this.ghostText) || void 0 === e ? void 0 : e.length) >
-//             0 &&
-//             this.ghostText.forEach(function (e) {
-//               r.ghostSingleText(e);
-//             });
-//         },
-//       },
-//       {
-//         key: 'ghostSingleText',
-//         value: function (e) {
-//           var t,
-//             n = new c.C(e, {
-//               type: 'words,chars',
-//             });
-//           (t = n.chars),
-//             u.ZP.set(t, {
-//               autoAlpha: 0,
-//             });
-//           var i = new u.ZP.timeline({
-//             paused: !0,
-//           }).fromTo(
-//             t,
-//             {
-//               autoAlpha: 0,
-//             },
-//             {
-//               autoAlpha: 1,
-//               duration: 0.2,
-//               ease: 'power2.in',
-//               stagger: {
-//                 each: 0.01,
-//                 from: 'random',
-//               },
-//             }
-//           );
-//           w.i.create({
-//             trigger: e,
-//             start: 'top 90%',
-//             onEnter: function () {
-//               i.play(0);
-//             },
-//             onEnterBack: function () {
-//               i.play(0);
-//             },
-//             onLeave: function () {
-//               u.ZP.to(t, {
-//                 autoAlpha: 0,
-//               });
-//             },
-//             onLeaveBack: function () {
-//               u.ZP.to(t, {
-//                 autoAlpha: 0,
-//               });
-//             },
-//           });
-//         },
-//       },
-//     ])
-//   );
-// };
+/* common */
+function getRandomArrays(len, num) {
+  let arrays = [];
+  for (let j = 0; j < num; j++) {
+    let arr = [];
+    while (arr.length < len) {
+      let randomNum = Math.floor(Math.random() * (len * num));
+      if (
+        !arrays.some((a) => a.includes(randomNum)) &&
+        !arr.includes(randomNum)
+      ) {
+        arr.push(randomNum);
+      }
+    }
+    arrays.push(arr);
+  }
+  return arrays;
+}
 
 const domSections = [
   {
     section: null,
     DOMRect: { height: 0 },
-    action: false,
+    state: false,
   },
 ];
-const sectionBounding = (e) => {
-  const sections = document.querySelectorAll('.sections');
-  // 돔 배열 초기화
-  return sections.forEach((section, i) => {
-    const DOMRect = section.getBoundingClientRect();
+
+const init = function () {
+  sectionBounding();
+  setRandomFade();
+  sectionScrollCheck();
+  floatMenu();
+
+  window.addEventListener('resize', sectionBounding);
+  window.addEventListener('scroll', () => {
+    sectionBounding();
+    sectionScrollCheck();
+    floatMenu();
+  });
+
+  document.querySelector('.btn_top').addEventListener('click', function () {
+    window.scrollTo(1000, 0);
+  });
+
+  window.addEventListener('mousemove', logoParallax);
+};
+
+const sectionBounding = () => {
+  const DOM = [
+    {
+      class: '.sec01',
+    },
+    {
+      class: '.sec02',
+    },
+    {
+      class: '.sec10',
+    },
+  ];
+  return DOM.forEach((section, i) => {
+    const DOMRect = document
+      .querySelector(section.class)
+      .getBoundingClientRect();
     domSections[i] = { ...domSections[i], DOMRect };
   });
 };
 
-const sectionScrollCheck = (scroll) => {
+const sectionScrollCheck = () => {
+  const innerHieght = parseInt(window.innerHeight);
+  const innerHieght2 = parseInt(window.innerHeight / 2);
   const innerHieght3 = parseInt(window.innerHeight / 3);
   const innerHieght4 = parseInt(window.innerHeight / 4);
-
-  // console.log(domSections[1].DOMRect.y - innerHieght4 <= window.scrollY); // 들어가는거
-  // console.log(domSections[1].DOMRect.bottom - innerHieght4 <= window.scrollY); // 나가는거
-
-  const scrolled =
-    window.pageYOffSet - innerHieght3 || window.scrollY - innerHieght3; // 섹션의 시작점에서 스크롤된 거리를 계산
   return domSections.forEach((section, i) => {
-    const inner = domSections[i].DOMRect.y - innerHieght4 <= window.scrollY; // 섹션 뷰포트내로 진입하는지 체크
-    const outte =
-      domSections[i].DOMRect.bottom - innerHieght4 <= window.scrollY; // 섹션 뷰포트내로 나가는지 체크
+    const dom = domSections[i];
+    const inner = dom.DOMRect.y - innerHieght2 <= 0; // 섹션 뷰포트내로 들어오는지 체크
+    const outte = dom.DOMRect.bottom <= 0; // 섹션 뷰포트내로 나가는지 체크
 
-    if (!domSections[i].action && inner && !outte) {
-      // 섹션 뷰포트 들어오는 액션
-      domSections[i].action = true;
-      return;
+    if (inner == outte && dom.state == true) {
+      dom.state = false;
+      // 끝나는액션
+      sectionScrollAct(dom.state, i);
     }
 
-    if (domSections[i].action && inner && outte) {
-      // 섹션 뷰포트 나가는 액션
-      domSections[i].action = false;
-      return;
+    // false , true, !false ,false
+    if (inner == !outte && !dom.state == true) {
+      dom.state = true;
+      // 시작하는액션
+      sectionScrollAct(dom.state, i);
     }
-
-    // domSections[i].action = !inner && outte ? false : true;
-
-    sectionScrollActCheck(section.DOMRect.top >= scrolled, section.section);
   });
 };
 
-const sectionScrollActCheck = (state, section) => {
-  // array.findIndex(obj => obj.name == "희동이")
-  // const idx = domSections.findIndex((obj) => obj.section == section);
-  // console.log(state, section);
-  const a = document.querySelector('.sec01'),
-    b = document.querySelector('.sec02');
-  switch (section) {
-    case b:
-      randomFade(state, section);
+const sectionScrollAct = (state, i) => {
+  // console.log(state, i);
+  switch (i) {
+    case 0:
+      randomFade(state);
+
+      break;
+    case 1:
+      randomFadeSen(state);
+
+      break;
+
+    case 2:
+      randomFade10(state);
+      imageSlideShow(state);
       break;
 
     default:
@@ -144,113 +112,164 @@ const sectionScrollActCheck = (state, section) => {
   }
 };
 
-const randomFade = (state, section) => {
-  // console.log(state, section);
+const setRandomFade = () => {
+  const selectors = document.querySelectorAll('.js-random-fade > span');
+
+  selectors.forEach((selector) => {
+    const text =
+      '<div style="opacity:0;">' +
+      [...selector.textContent].join('</div><div style="opacity:0;">') +
+      '</div>';
+    selector.innerHTML = text;
+  });
 };
-const script = (function () {
-  window.addEventListener('load', sectionBounding);
-  window.addEventListener('resize', sectionBounding);
-  window.addEventListener('scroll', sectionBounding, sectionScrollCheck);
 
-  // window.addEventListener('scroll', function () {
-  //   // 스크롤 이벤트 리스너 등록
+const randomFade = (state) => {
+  const randomArrays = getRandomArrays(20, 4);
+  // const parents = document.querySelector('.sec01');
+  // const parent = parents.querySelectorAll('.js-random-fade span');
+  const selectors = document.querySelectorAll('.js-random-fade span > div');
 
-  //   sections.forEach(function (section) {
-  //     let bounds = section.getBoundingClientRect(); // 섹션의 위치와 크기 정보를 가져옴
-  //     if (section === document.querySelector('.sec02')) {
-  //     }
+  const opacityOn = (i) => {
+    selectors[i].style.opacity = 1;
+  };
+  if (state) {
+    // parent.forEach((node, i) => {
+    //   node.style.opacity = 1;
+    // });
+    randomArrays.forEach((arr, i) => {
+      setTimeout(() => {
+        arr.forEach(opacityOn);
+      }, 100 * i + 500);
+    });
+  }
 
-  //     // bounds.bottom >= 0
-  //     // if (
-  //     //   bounds.top >= window.innerHeight &&
-  //     //   window.innerHeight < window.innerHeight
-  //     // ) {
-  //     //   // 섹션이 뷰포트 내에 있을 때
-  //     //   var scrolled =
-  //     //     (window.pageYOffSet ||
-  //     //       window.scrollY ||
-  //     //       document.documentElement.scrollTop) - section.offsetTop; // 섹션의 시작점에서 스크롤된 거리를 계산
+  // setTimeout(() => {
+  //   if (!state) {
+  //     parent.forEach((node, i) => {
+  //       node.style.opacity = 0;
+  //     });
+  //     selectors.forEach((selector) => {
+  //       selector.style.opacity = 0;
+  //     });
+  //   }
+  // }, 1000);
 
-  //     //   randomShowText(section);
-  //     //   // background.style.transform = `translateY(${scrolled * 0.8}px)`; // 제목을 스크롤 속도의 80%(빠르게)로 이동
-  //     //   // title.style.transform = `translateY(${scrolled * 0.3}px)`; // 배경을 스크롤 속도의 30%(느리게)로 이동
-  //     //   // circle.style.transform = `translate(${scrolled * 0.5}px, ${scrolled * 0.5}px)`; // circle 왼쪽에서 오른쪽으로 이동
-  //     //   // square.style.transform = `translate(${scrolled * -0.5}px, ${scrolled * 0.5}px)`; // square 오른쪽에서 왼쪽으로 이동
-  //     // }
+  return;
+};
 
-  //     let enter = bounds.top <= window.innerHeight / 2;
+const randomFadeSen = (state) => {
+  const randomArrays = [1, 4, 2, 7, 6, 9, 10, 11, 0, 3, 5, 8];
+  // const parents = document.querySelector('.sec02');
+  // const parent = parents.querySelectorAll('.js-random-fade span');
+  const selectors = document.querySelectorAll(
+    '.js-random-fade-sen > span,.js-random-fade-sen > strong'
+  );
 
-  //     if (!enter) {
-  //       randomTextHide();
-  //       return;
-  //     }
-  //     randomTextShow();
-  //   });
-  // });
-  // function randomTextShow() {
-  //   console.log('show');
-  //   TweenMax.to(
-  //     $('.js-ghost-text div'),
-  //     { opacity: 1, duration: 0.2 },
-  //     { ease: 'power2.in' }
-  //   );
-  // }
-  // function randomTextHide(e) {
-  //   console.log('hide');
-  //   TweenMax.to(
-  //     $('.js-ghost-text div'),
-  //     { opacity: 0, duration: 0.2 },
-  //     { ease: 'power2.in' }
-  //   );
-  // }
-})();
+  const opacityOn = (i, u) => (selectors[i].style.opacity = 1);
 
-// const script = () => {
-//   function e() {
-//     !(function (e, t) {
-//       if (!(e instanceof t))
-//         throw new TypeError('Cannot call a class as a function');
-//     })(this, e),
-//       (this.DOM = {
-//         ghostText: '.js-ghost-text',
-//       }),
-//       (this.ghostText = document.querySelectorAll(this.DOM.ghostText)),
-//       this.init();
-//   }
+  if (state) {
+    // parent.forEach((node, i) => {
+    //   node.style.opacity = 1;
+    // });
+    randomArrays.forEach((arr, i) => {
+      setTimeout(() => opacityOn(arr), 50 * i);
+    });
+  }
 
-//   // window.addEventListener('scroll', () => {
-//   //   let scrollTop =
-//   //     window.pageYOffSet ||
-//   //     window.scrollY ||
-//   //     document.documentElement.scrollTop;
+  // setTimeout(() => {
+  //   if (!state) {
+  //     parent.forEach((node, i) => {
+  //       node.style.opacity = 0;
+  //     });
+  //     selectors.forEach((selector) => {
+  //       selector.style.opacity = 0;
+  //     });
+  //   }
+  // }, 1000);
 
-//   //   const ghostText = document.querySelector('.js-ghost-text');
-//   //   if (scrollTop >= ghostText.scrollTop) {
-//   //     return;
-//   //   }
-//   // });
+  return;
+};
 
-//   return {
-//     DOM: {
-//       ghostText: '.js-ghost-text > div',
-//     },
-//     ghostText: document.querySelectorAll('.js-ghost-text div'),
-//     init: function () {
-//       var e,
-//         r = this;
-//       (null === (e = this.ghostText) || void 0 === e ? void 0 : e.length) > 0 &&
-//         this.ghostText.forEach(function (e) {
-//           r.ghostSingleText(e);
-//         });
-//     },
-//     ghostSingleText: function (e) {
-//       if (!this.On) {
-//         console.log('1');
-//         TweenMax.to(e, { opacity: 1, duration: 0.2 }, { ease: 'power2.in' });
-//         return;
-//       }
-//       TweenMax.to(e, { opacity: 0, duration: 0.2 }, { ease: 'power2.in' });
-//       console.log('2');
-//     },
-//   };
-// };
+const randomFade10 = (state) => {
+  const randomArrays = getRandomArrays(4, 4);
+  const parents = document.querySelector('.sec10');
+  // const parent = parents.querySelectorAll('.js-random-fade span');
+  const selectors = document.querySelectorAll(
+    '.area_h.js-random-fade span > div'
+  );
+  const opacityOn = (i) => {
+    console.log(selectors);
+    selectors[i].style.opacity = 1;
+  };
+  if (state) {
+    // parent.forEach((node, i) => {
+    //   node.style.opacity = 1;
+    // });
+    randomArrays.forEach((arr, i) => {
+      setTimeout(() => {
+        arr.forEach(opacityOn);
+      }, 150 * i);
+    });
+    return;
+  }
+
+  // setTimeout(() => {
+  //   if (!state) {
+  //     parent.forEach((node, i) => {
+  //       node.style.opacity = 0;
+  //     });
+  //     selectors.forEach((selector) => {
+  //       selector.style.opacity = 0;
+  //     });
+  //   }
+  // }, 1000);
+
+  return;
+};
+
+const imageSlideShow = (state) => {
+  const selector = document.querySelector('.js-image-slideshow');
+  if (state) {
+  }
+};
+
+const floatMenu = () => {
+  floatSuggestButton();
+  floatTopButton();
+};
+
+const floatSuggestButton = (e) => {
+  const floatButton = $('.btn_suggset.js-float');
+  const inFoot =
+    window.scrollY > document.body.scrollHeight - window.innerHeight * 2;
+  if (inFoot) {
+    floatButton.stop().fadeOut('fast');
+    return;
+  }
+  floatButton.stop().fadeIn('fast');
+};
+const floatTopButton = (e) => {
+  const floatButton = $('.area_float');
+  if (window.scrollY > window.innerHeight) {
+    floatButton.stop().fadeIn('fast');
+    return;
+  }
+  floatButton.stop().fadeOut('fast');
+};
+
+const logoParallax = (e) => {
+  const x = (window.innerWidth - e.pageX) / 100;
+  const y = (window.innerHeight - e.pageY) / 300;
+  const xx = x / 1.1;
+  const yy = y / 1.1;
+
+  document.querySelector(
+    '.img_overEffect'
+  ).style.transform = `translateX(${x}px) translateY(${y}px)`;
+  document.querySelector(
+    '.js-parallax'
+  ).style.transform = `translateX(${xx}px) translateY(${yy}px)`;
+};
+
+window.addEventListener('load', init);
